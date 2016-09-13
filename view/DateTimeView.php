d965f313da6b62d55be61866cc6116e1638ec465
Loke Carlsson <lc222ak@student.lnu.se>
2016-09-13 13:11:38 +0200

    Added date

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l") . ", the " . date("d") . "th of " . date("F") . ", The time is " . date("H:i");
 
 		return '<p>' . $timeString . '</p>';
 	}
--- view/LayoutView.php
+++ view/LayoutView.php
@@ -2,28 +2,28 @@
 
 
 class LayoutView {
-  
+
   public function render($isLoggedIn, LoginView $v, DateTimeView $dtv) {
     echo '<!DOCTYPE html>
       <html>
         <head>
           <meta charset="utf-8">
-          <title>Login Example</title>
+          <title>1DV610</title>
         </head>
         <body>
           <h1>Assignment 2</h1>
           ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          
+
           <div class="container">
               ' . $v->response() . '
-              
+
               ' . $dtv->show() . '
           </div>
          </body>
       </html>
     ';
   }
-  
+
   private function renderIsLoggedIn($isLoggedIn) {
     if ($isLoggedIn) {
       return '<h2>Logged in</h2>';
