377c140b8b5ac8f931dda7273189168bef002252
Loke Carlsson <lc222ak@student.lnu.se>
2016-09-23 11:12:32 +0200

    Fixed date to display suffixes for the day

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date("l") . ", the " . date("d") . "th of " . date("F") . " " . date("Y") . ", The time is " . date("H:i:s");
+		$timeString = date("l") . ", the " . date("dS") . " of " . date("F") . " " . date("Y") . ", The time is " . date("H:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
